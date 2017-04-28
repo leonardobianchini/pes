2247aee75b11273eec730a711346f18f7d5d145a
Aluno (local) <andre.luiz_1985@hotmail.com>
2017-04-27 22:08:16 -0300

    escopo dos formularios

---
+++ escopo Formularios/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<form action="formulario.php" method="POST">
+			Nome : <input type="text" name="nome"><br>
+			E-mail : <input type="text" name="email"><br> 
+			Senha: <input type="password" name="senha"><br>
+			<input type="submit" value="Cadastrar">
+		</form>
+	</body>
+</html>
