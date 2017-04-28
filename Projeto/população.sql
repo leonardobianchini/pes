654eedfec64199c86a66c957821156397e13c13e
leonardobianchini <leonardobianchini7@gmail.com>
2017-04-27 22:18:05 -0300

    Criado arquivo de populacao de tabelas

---
+++ Projeto/população.sql
@@ -0,0 +1,43 @@
+insert into desportista values
+('Gabriel Jesus', '1996/08/19', 11999872156, 'gabrielj@gmail.com', '123'),
+('Jonathan dos Santos', '1993/10/23', 11999459863, 'jonsantos@gmail.com', 'abcd'),
+('Artur Morais', '1983/09/01', 49995861237, 'arturm@gmail.com', 'cchap'),
+('Edivandro Rossi', '1991/01/30', 49994596335, 'rossi@hotmail.com', 'qat12'),
+('Andres Dalessandro', '1981/04/2', 55994132589, 'dalessandro@gmail.com', 'int10'),
+('Marcelo Debiassi', '1988/08/13', 49562656626, 'mdb@bol.com', 'gf55fd'),
+('Bruno Miranda', '1974/03/12', 499936556962, 'bmiranda@gmail.com', 'sas565'),
+('Pedro Augusto', '1957/06/25', 499996528262, 'paugusto@gmail.com', '4dfd'),
+('Paulo Nobre', '1976/11/24', 11982366156, 'pnobre@hotmail.com', 'ad5f4g'),
+('Fernando Souza', '1980/12/25', 11958622644, 'fersouza@gmail.com', 'wqwq84'),
+('Fabricio Bruno', '1978/01/01', 11997899516, 'fabriciobruno@gmail.com', 'ty5hg1'),
+('Osvaldo Firmino', '1986/03/10', 19366541820, 'firmino@gmail.com', 'olases'),
+('Neymar da Silva', '1993/10/11', 111001125611, 'ousadiaealegria@gmail.com', 'ehtois');
+
+insert into organizador values
+('Januario Silva', '1961/06/19', 499935851652, 'januariosilva@gmail.com', '998sw'),
+('Felix Pinheiro', '1970/12/21', 499992365463, 'fpinheiro@hotmail.com', '12124'),
+('Dorival Prieto', '1956/08/19', 11999872156, 'dorival@gmail.com', 'cfv4d'),
+('Osmar Vadao', '1961/11/02', 45996645230, 'osmarvadao@gmail.com', 'ed43tfd'),
+('Valdir Ramos', '1973/02/21', 49669562262, 'vdramos@gmail.com', '43tgg');
+
+insert into administrador values
+('administrador', 'adm@adm.com', 'adm');
+
+insert into local values
+('Rua dos coqueiros', 135, 'Bairro Palmeiras', 'Chapeco', 'Campo', '16:00 - 23:30'),
+('Rua Clevelandia', 100, 'Centro', 'Chapeco', 'Quadra', '18:00 - 23:30'),
+('Rua Antonio Ramos', 135, 'Centro', 'Chapeco', 'Piscina', '08:00 - 20:00'),
+('Rua Barao do Rio Branco', 135, 'Centro', 'Chapeco', 'Quadra', '13:00 - 21:00');
+
+insert into esporte values
+('Futebol'),
+('Futsal'),
+('Polo aquatico'),
+('Tenis'),
+('Basquete'),
+('Volei');
+
+insert into evento values
+('Rua Clevelandia', 'Felix Pinheiro', '19:00 - 20:00', 'Futsal'),
+('Rua Clevelandia', 'Osmar Vadao', '21:00 - 22:00', 'Volei'),
+('Rua Barao do Rio Branco', 'Valdir Ramos', '14:00 - 15:00', 'Tenis');
