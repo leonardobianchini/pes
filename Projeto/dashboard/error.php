91e5b8e2a22ced7c75aae0ffc11f1090866f8bf3
leonardobianchini <leonardobianchini7@gmail.com>
2017-06-01 22:08:58 -0300

    Layout V. parte 1 hehhe

---
+++ Projeto/dashboard/error.php
@@ -0,0 +1,20 @@
+<div id="content">
+  <div class="container-fluid">
+    <div class="row-fluid">
+      <div class="span12">
+        <div class="widget-box">
+          <div class="widget-title"> <span class="icon"> <i class="icon-info-sign"></i> </span>
+            <h5><?php echo "Error $err" ?></h5>
+          </div>
+          <div class="widget-content">
+            <div class="error_ex">
+              <h1><?php echo "Error $err" ?></h1>
+              <h3>Opps... Você parece estar perdido!</h3>
+              <p>Não foi possível encontrar a página requisitada.</p>
+              <a class="btn btn-warning btn-big"  href="/Projeto/dashboard/?pg=inicio">Voltar ao início</a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
