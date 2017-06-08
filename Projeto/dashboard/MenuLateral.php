2903015369449877360f591ce67accdc587dc166
leonardobianchini <leonardobianchini7@gmail.com>
2017-06-08 20:28:48 -0300

    Criado formulario para cadastro de evento

--- Projeto/dashboard/MenuLateral.php
+++ Projeto/dashboard/MenuLateral.php
@@ -1,7 +1,7 @@
 
 <div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Página Inicial</a>
   <ul>
-    <li class="active"><a href="index.html"><i class="icon icon-home"></i> <span>Página Inicial</span></a> </li>
+    <li class="active"><a href="index.php"><i class="icon icon-home"></i> <span>Página Inicial</span></a> </li>
     <li> <a href="charts.html"><i class="icon icon-signal"></i> <span>Frequência de Atividades</span></a> </li>
     <li> <a href="widgets.html"><i class="icon icon-inbox"></i> <span>Equipes</span></a> </li>
     <li><a href="tables.html"><i class="icon icon-th"></i> <span>Próximos Torneios</span></a></li>
@@ -11,6 +11,7 @@
         <li><a href="form-common.html">Grupo</a></li>
         <li><a href="form-validation.html">Ambiente</a></li>
         <li><a href="form-wizard.html">Torneio</a></li>
+        <li><a href="evento.php">Evento</a></li>
       </ul>
     </li>
     </li>
---
+++ Projeto/dashboard/formularioevento.php
@@ -0,0 +1,54 @@
+event<div id="content">
+<!--breadcrumbs-->
+	<div id="content-header">
+		<div id="breadcrumb"> <a href="index.php" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a></div>
+	</div>
+<!--End-breadcrumbs-->
+    <hr/>
+    <div class="row-fluid">
+		<div class="widget-box">
+			<div class="widget-title"> <span class="icon"> <i class="icon-align-justify"></i> </span>
+			<h5>Cadastrar Evento</h5>
+		</div>
+		<div class="widget-content nopadding">
+			<form action="#" method="get" class="form-horizontal">
+			<div class="control-group">
+				<label class="control-label">Endereço :</label>
+				<div class="controls">
+					<input type="text" class="span11" placeholder="Endereço" />
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label">Organizador :</label>
+				<div class="controls">
+					<input type="text" class="span11" placeholder="Organizador" />
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label">Data :</label>
+				<div class="controls">
+					<input type="password"  class="span11" placeholder="Data"  />
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label">Horário :</label>
+				<div class="controls">
+					<input type="text" class="span11" placeholder="Horário" />
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label">Esporte :</label>
+				<div class="controls">
+					<input type="text" class="span11" placeholder="Esporte" />
+				</div>
+			</div>
+			<div class="control-group">
+				<div class="controls">
+					<button>Enviar</button>
+					<button>Cancelar</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
