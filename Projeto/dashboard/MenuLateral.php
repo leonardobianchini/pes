c7153bda35bf0397ee37349878ce79c717de06c7
Andreluiz04 <andre.luiz_1985@hotmail.com>
2017-06-08 21:47:08 -0300

    lol index

--- Projeto/dashboard/MenuLateral.php
+++ Projeto/dashboard/MenuLateral.php
@@ -4,16 +4,15 @@
     <li class="active"><a href="index.php?pg=inicio"><i class="icon icon-home"></i> <span>Página Inicial</span></a> </li>
     <li> <a href="index.php?pg=freq"><i class="icon icon-signal"></i> <span>Frequência de Atividades</span></a> </li>
     <li> <a href="index.php?pg=equipe"><i class="icon icon-inbox"></i> <span>Equipes</span></a> </li>
-    <li><a href="index.php?pg=ptor"><i class="icon icon-th"></i> <span>Próximos Torneios</span></a></li>
+    <li><a href="index.php?pg=ptor"><i class="icon icon-th"></i> <span>Próximos Eventos</span></a></li>
     <li><a href="index.php?pg=esporte"><i class="icon icon-fullscreen"></i> <span>Esportes</span></a></li>
-    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Inserir novo..</span> <span class="label label-important">4</span></a>
+    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Inserir novo..</span> <span class="label label-important">3</span></a>
       <ul>
         <li><a href="index.php?pg=grupo">Grupo</a></li>
         <li><a href="index.php?pg=ambiente">Ambiente</a></li>
-        <li><a href="index.php?pg=torneio">Torneio</a></li>
         <li><a href="index.php?pg=evento">Evento</a></li>
       </ul>
     </li>
     </li>
   </ul>
-</div
+</div>
--- Projeto/dashboard/index.php
+++ Projeto/dashboard/index.php
@@ -57,9 +57,6 @@ switch ($pg) {
   case 'ambiente':
     include 'Ambiente.php';
     break;
-  case 'torneio':
-    include 'Torneio.php';
-    break;
   case 'freq':
     include 'Frequencia_Atividades.php';
     break;
@@ -82,7 +79,7 @@ switch ($pg) {
 <!--Footer-part-->
 
 <div class="row-fluid">
-  <div id="footer" class="span12"> 2013 &copy; Matrix Admin. Brought to you by <a href="http://themedesigner.in">Themedesigner.in</a> </div>
+  <div id="footer" class="span12"> 2017 &copy; Todos os direitos reservados</a> </div>
 </div>
 
 <!--end-Footer-part-->
